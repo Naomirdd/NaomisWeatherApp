fdc668df2f747ca5132dafa8e69e1ae08a3fdfe5
Naomirdd <riddnaomi1@gmail.com>
2024-01-08 15:17:51 +0000

    Finishing the search form

--- src/index.java
+++ src/index.java
@@ -1,1 +1,11 @@
+
+function handleSearch(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("search-form-input");
+    let cityElement = document.querySelector("weather-app-city");
+    cityElement.innerHTML= searchInput.value;
+    }
+
 Let searchFormElement = document.querySelector("search-form");
+
+searchFormElement.addEventListener("submit" handleSearch );
